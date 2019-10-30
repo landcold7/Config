6534d1297728ba3200fa1317bc5028562e0dc643
landcold7 <landcold7@gmail.com>
2019-10-30 18:35:18 +0800

    Update launchd services

--- mac/Library/LaunchAgents/me@.autossh.ict.sh
+++ mac/Library/LaunchAgents/me@.autossh.ict.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -x
 set -e
+
+pkill autossh
 if ! autossh -M 0 -f -T -N ict; then
   exit 1
 fi
--- mac/Library/LaunchAgents/me@.system.update.sh
+++ mac/Library/LaunchAgents/me@.system.update.sh
@@ -8,7 +8,7 @@ action "\n\n\n====Update system $(date)===="
 
 # Upgrade homebrew itself
 action "\n› brew update"
-brew update
+brew update 2>&1
 
 # Run Homebrew through the Brewfile
 # Install or upgrade all packages
@@ -18,4 +18,4 @@ action "\n› brew bundle"
 
 # Update mac app store
 action "\n› softwareupdate -i -a"
-softwareupdate -i -a
+softwareupdate -i -a 2>&1
