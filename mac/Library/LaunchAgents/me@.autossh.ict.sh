cb1428ff1e0d11b484d01b67e8da70378fb6d607
landcold7 <landcold7@gmail.com>
2019-10-31 23:30:33 +0800

    Add a log.sh for logging stuff for shell script

--- mac/Library/LaunchAgents/me@.autossh.ict.sh
+++ mac/Library/LaunchAgents/me@.autossh.ict.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-set -x
 set -e
+source ~/Util/log.sh
 
+log info "starting autossh service"
 pkill autossh
 if ! autossh -M 0 -f -T -N ict; then
   exit 1
--- mac/Library/LaunchAgents/me@.system.update.sh
+++ mac/Library/LaunchAgents/me@.system.update.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
+set -e
+source ~/Util/log.sh
 
-action() {
-  printf "\e[1;32m$*\e[m\n"
-}
-
-action "\n\n\n====Update system $(date)===="
+log info "starting sysmte update service"
 
 # Upgrade homebrew itself
-action "\n› brew update"
+log info "brew update"
 brew update 2>&1
 
 # Run Homebrew through the Brewfile
 # Install or upgrade all packages
 # TODO(zq7): complete this.
-action "\n› brew bundle"
+log info "brew bundle"
 # brew bundle -v --file ~/core ~/user-app ~/graphic-app etc..
 
 # Update mac app store
-action "\n› softwareupdate -i -a"
+log info "softwareupdate -i -a"
 softwareupdate -i -a 2>&1
