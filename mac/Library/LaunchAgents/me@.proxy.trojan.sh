d8508c96ba58e74fda024b9db016e2ada52c3812
landcold7 <landcold7@gmail.com>
2019-11-27 13:27:37 +0800

    Fix trojan service

--- mac/Library/LaunchAgents/me@.proxy.trojan.sh
+++ mac/Library/LaunchAgents/me@.proxy.trojan.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 source ~/Util/log.sh
 
-log info "starting autossh service"
+log info "starting trojan proxy service"
 
+export PATH=~/bin:$PATH
 pkill trojan
-if ! trojan .config/trojan-client.json ; then
+if ! trojan ~/.config/trojan-client.json ; then
   exit 1
 fi
